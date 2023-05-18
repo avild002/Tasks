f694c11c22cc638dc24188986591a72a6b78780e
Claudia Martínez Monroy <>
2023-05-17 20:35:56 -0400

    Se crea el objeto tarea.

---
+++ src/create.js
@@ -0,0 +1,28 @@
+import { task } from "./src/task.js";
+import { listTask } from "./src/list.js";
+
+
+export function createTask(taskName,priorityTask) {
+    //creo un arreglo para guardar los objetos.
+    const list = [];
+    // Creo un objeto y lo agrego a un array
+    list.push(new task(taskName,priorityTask));
+    showMessage();
+    orderList(list);
+
+}
+
+// TODO - No me está organizando el array de objetos por prioridad.  
+function orderList (list){
+    const sortList = list.sort((a,b) => a[0].getPriority() > b[1].getPriority() ? 1 : -1); 
+    listTask(sortList)
+}
+// TODO -  Falta incluir un temporizador al alert.
+function showMessage(){
+    const mesageOk = document.getElementById("message");
+    const element = document.createElement("div");
+    element.innerHTML = `<div class="alert alert-success d-flex align-items-center" role="alert">
+    <div> Se ha creado la tarea correctamente.</div></div>`
+    mesageOk.appendChild(element);
+    
+} 
