9e340f6be8bb61e74c0601c407cbf2cdd6b1a612
Claudia Martínez Monroy <>
2023-05-19 17:21:17 -0400

    Agrego las comillas para el atributo id del boton editar y eliminar.

--- src/list.js
+++ src/list.js
@@ -5,14 +5,14 @@ export function listTask (list,index) {
     const listTask = document.getElementById("task-list");
     const element = document.createElement("div");
     element.innerHTML = `
-    <div class="container py-2 text-center mb-8">
+    <div class="container py-2 text-center mb-8" id="lista">
         <div class ="container py-2 p-3 mb-2 bg-light text-dark">
             <strong>Tarea :</strong> <input type="text" value = ${list[index].name} disabled>
             <strong>prioridad :</strong> <input type="text" value = ${list[index].priority} disabled> 
             <strong>Estado :</strong> <input class="form-check-input" type="checkbox" value=""> ${list[index].state}
         <div><br>
-        <button type="button" id= ${index} name ="Editar" class="btn btn-info">Editar</button>
-        <button type="button" id= ${index} name = "Eliminar" class="btn btn-danger">Eliminar</button>
+        <button type="button" id="${index}" name ="Editar" class="btn btn-info">Editar</button>
+        <button type="button" id="${index}" name = "Eliminar" class="btn btn-danger">Eliminar</button>
     </div>`;
     listTask.appendChild(element);
 
