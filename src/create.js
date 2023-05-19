ec68a58d798eaca712cf4fa69934dae67244566a
Claudia Martínez Monroy <>
2023-05-19 17:20:08 -0400

    Cambio el mensaje de correcto por un alert.

--- src/create.js
+++ src/create.js
@@ -7,12 +7,10 @@ export function createTask(taskName,priorityTask) {
     var index = 0; 
     // Creo un objeto y lo agrego a un array
     list.push(new task(taskName,priorityTask));
-    showMessage();
+    alert("Se ha creado la tarea correctamente..!!");
     index = list.length - 1; 
-    //orderList(list,index);
     listTask(list,index);
-    console.log(list);
-    
+
 }
 
 //TODO -  ya funciona el orderList solo falta revisar como pintar en index.html las tareas por la prioridad. 
@@ -20,16 +18,4 @@ function orderList (list, index){
     if (list.length >= 1) {
         list.sort((a, b) => b.getPriority() - a.getPriority()); 
     }
-    
-
 }
-
-// TODO -  Falta incluir un temporizador al alert.
-function showMessage(){
-    const mesageOk = document.getElementById("message");
-    const element = document.createElement("div");
-    element.innerHTML = `<div class="alert alert-success d-flex align-items-center " role="alert">
-    <div> Se ha creado la tarea correctamente.</div></div>`
-    mesageOk.appendChild(element);
-    
-} 
