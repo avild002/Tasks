0e2ca55eee994f5e87c1e36724fe37ca0b07e1cd
Claudia Martínez Monroy <>
2023-05-19 17:22:22 -0400

    Se agrega función edit.

---
+++ src/edit.js
@@ -0,0 +1,7 @@
+// TODO - Falta habilitar los campos de la tarea y traer el array para actualizarlo
+export function editTask(element) {
+    if (element.name === "Editar") {
+      //element.parentElement.parentElement.remove();
+      console.log("editandooo");
+    }
+}
