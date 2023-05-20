c5d76b744a54df3931f30a0a6f031f210b4d922c
Claudia Martínez Monroy <>
2023-05-20 11:47:38 -0400

    Se deja console.log

--- src/delete.js
+++ src/delete.js
@@ -4,6 +4,7 @@
 export function deleteTask(element,list) {
         if (element.name === "Eliminar") {
           list.splice(element.id, 1);
+          console.log(list);
           element.parentElement.parentElement.remove();
         }
 
