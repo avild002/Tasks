3431cb3b22e21f47dbf88895cd2098453365ef94
Claudia Martínez Monroy <>
2023-05-23 13:25:34 -0400

    Se crea funcionalidad editar.

--- src/edit.js
+++ src/edit.js
@@ -1,7 +1,67 @@
-// TODO - Falta habilitar los campos de la tarea y traer el array para actualizarlo
+
 export function editTask(element) {
+        
     if (element.name === "Editar") {
-      //element.parentElement.parentElement.remove();
-      console.log("editandooo");
+        // habilitar cajas de texto 
+        var indexTask = element.id; 
+        console.log("Valor de index en editar:   "); 
+        console.log(indexTask); 
+        activeInput(indexTask);
+        createBtn(indexTask);
+        const btnEdit = document.getElementById(`${indexTask}`); 
+        btnEdit.disabled = true;   
     }
+
+}
+
+export function updateTask(element, list) {
+  
+    if (element.name === "update") {
+        var indexBtn = element.id;
+        console.log("Update"); 
+        console.log(indexBtn); 
+        const newNameTask = document.getElementById(`task_+${indexBtn}`).value;
+        const newPrioTask = document.getElementById(`prio_+${indexBtn}`).value;
+        console.log("Nuevo nombre "+ newNameTask); 
+        console.log("Nueva prio "+ newPrioTask); 
+        list[indexBtn].setName(newNameTask);
+        list[indexBtn].setPriority(newPrioTask);
+        console.log(list);
+        alert("Se han actualizado los campos correctamente..!!"); 
+        element.style.display = "none";
+        const btnEdit = document.getElementById(`${indexBtn}`); 
+        btnEdit.disabled = false;   
+        inactivateInput(indexBtn); 
+    
+    }
+  
+}
+
+function activeInput (index) {
+
+    const nameTask = document.getElementById(`task_+${index}`);
+    nameTask.disabled = false;  
+    const prioTask = document.getElementById(`prio_+${index}`);
+    prioTask.disabled = false; 
+
+}
+
+function createBtn (indexTask) {
+
+    const btn = document.getElementById("task-list");
+    const element = document.createElement("div");
+    element.innerHTML = `
+        <button type="button" id="${indexTask}" name ="update" class="btn btn-secondary">Update</button>
+    `;
+    btn.appendChild(element);
+
+}
+
+function inactivateInput (indexBtn) {
+  
+    const nameTask = document.getElementById(`task_+${indexBtn}`);
+    nameTask.disabled = true;  
+    const prioTask = document.getElementById(`prio_+${indexBtn}`);
+    prioTask.disabled = true; 
+
 }
