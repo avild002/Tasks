d9819c103478d8440e20ead324f966ead5ec7a48
Claudia Martínez Monroy <>
2023-06-12 19:47:10 -0400

    Se ajusta código.

--- src/delete.js
+++ src/delete.js
@@ -1,10 +1,10 @@
 
 export function deleteTask(element,list,index) {
     if (element.name === "Eliminar") {
-      const div_update = document.getElementById(`update_+${index}`);
-      div_update.remove(); 
       list.splice(index, 1);
       console.log(list);
+      const div_update = document.getElementById(`update_+${index}`);
+      div_update.remove(); 
       element.parentElement.parentElement.remove();
           
     }
